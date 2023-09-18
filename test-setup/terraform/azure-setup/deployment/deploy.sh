c3fd775b84e27957ba79bcace6df6c208bcf9c5e
ashiven <nevisha@pm.me>
2023-09-18 17:30:40 +0200

    Move services.txt file to the correct location

--- test-setup/terraform/azure-setup/deployment/deploy.sh
+++ test-setup/terraform/azure-setup/deployment/deploy.sh
@@ -12,7 +12,7 @@ checker_ip="108.142.137.253"
 
 scp -i ../data/test_key ../data/test_key groot@${checker_ip}:/home/groot/.ssh/test_key
 scp -i ../data/test_key ./services.sh groot@${checker_ip}:/home/groot/checkers.sh
-scp -i ../data/test_key ./services.txt groot@${vulnbox_ip}:/home/groot/services.txt
+scp -i ../data/test_key ./services.txt groot@${checker_ip}:/home/groot/services.txt
 
 engine_ip="108.142.138.9"
 
