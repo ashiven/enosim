42b85110a71144a076220d8e2fa3b4b74e09fd78
ashiven <nevisha@pm.me>
2023-09-19 15:46:35 +0200

    Rename checker.sh and services.sh to checker.sh and vulnbox.sh respectively in azure-setup/deployment folder

--- test-setup/terraform/azure-setup/deployment/deploy.sh
+++ test-setup/terraform/azure-setup/deployment/deploy.sh
@@ -5,13 +5,13 @@ cd "C:\Users\janni\OneDrive\Dokumente\Projects\Python\simulation-framework\enosi
 vulnbox_ip="108.142.137.233"
 
 scp -i ../data/test_key ../data/test_key groot@${vulnbox_ip}:/home/groot/.ssh/test_key
-scp -i ../data/test_key ./services.sh groot@${vulnbox_ip}:/home/groot/services.sh
+scp -i ../data/test_key ./vulnbox.sh groot@${vulnbox_ip}:/home/groot/vulnbox.sh
 scp -i ../data/test_key ./services.txt groot@${vulnbox_ip}:/home/groot/services.txt
 
 checker_ip="108.142.137.253"
 
 scp -i ../data/test_key ../data/test_key groot@${checker_ip}:/home/groot/.ssh/test_key
-scp -i ../data/test_key ./checkers.sh groot@${checker_ip}:/home/groot/checkers.sh
+scp -i ../data/test_key ./checker.sh groot@${checker_ip}:/home/groot/checker.sh
 scp -i ../data/test_key ./services.txt groot@${checker_ip}:/home/groot/services.txt
 
 engine_ip="108.142.138.9"
