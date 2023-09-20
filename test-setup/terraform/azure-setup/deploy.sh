10f0b47077a9f797f1342d9a22bacda2ec9a65a0
ashiven <nevisha@pm.me>
2023-09-20 21:59:52 +0200

    Update .gitignore and deploy.sh scripts

--- test-setup/terraform/azure-setup/deploy.sh
+++ test-setup/terraform/azure-setup/deploy.sh
@@ -12,10 +12,10 @@ if [ -n "${1-}" ] && [ "$1" == "-d" ]; then
   exit 0  
 fi
 
-echo "Building infrastructure ..."
-terraform init
-terraform validate
-terraform apply -auto-approve
+#echo "Building infrastructure ..."
+#terraform init
+#terraform validate
+#terraform apply -auto-approve
 terraform output | tee output.txt
 
 vulnbox_ip=$(grep -oP "vulnbox_ip = \K[^\s]+" ./output.txt)
@@ -32,15 +32,15 @@ echo "Configuring vulnbox ..."
 scp -F ${ssh_config} ./data/vulnbox.sh vulnbox:/home/groot/vulnbox.sh
 scp -F ${ssh_config} ./config/services.txt vulnbox:/home/groot/services.txt
 echo "This will take a few minutes. Please be patient."
-ssh -F ${ssh_config} vulnbox "chmod +x vulnbox.sh && ./vulnbox.sh" > ./logs/vulnbox_config.log
+ssh -F ${ssh_config} vulnbox "chmod +x vulnbox.sh && ./vulnbox.sh" > ./logs/vulnbox_config.log 2>&1
 
 echo "Configuring checker ..."
 scp -F ${ssh_config} ./data/checker.sh checker:/home/groot/checker.sh
 scp -F ${ssh_config} ./config/services.txt checker:/home/groot/services.txt
 echo "This will take a few minutes. Please be patient."
-ssh -F ${ssh_config} checker "chmod +x checker.sh && ./checker.sh" > checker_config.log
+ssh -F ${ssh_config} checker "chmod +x checker.sh && ./checker.sh" > checker_config.log 2>&1
 
 echo "Configuring engine ..."
 scp -F ${ssh_config} ./data/engine.sh engine:/home/groot/engine.sh
 scp -F ${ssh_config} ./config/ctf.json engine:/home/groot/ctf.json
-ssh -F ${ssh_config} engine "mkdir data && chmod +x engine.sh && ./engine.sh" > engine_config.log &
+ssh -F ${ssh_config} engine "mkdir data && chmod +x engine.sh && ./engine.sh" > engine_config.log 2>&1 &
