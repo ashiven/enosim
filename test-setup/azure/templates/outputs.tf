963427da0b805847d38672a6eda0ef5a3b81f9d3
ashiven <nevisha@pm.me>
2023-09-25 01:59:13 +0200

    Refactor outputs.tf to use dot notation for IP addresses

--- test-setup/azure/templates/outputs.tf
+++ test-setup/azure/templates/outputs.tf
@@ -1,12 +1,14 @@
 output "private_ip_addresses" {
-  value = [for _, nic in azurerm_network_interface.vm_nic : nic.ip_configuration[0].private_ip_address]
+  value = {
+    for name, vm in azurerm_network_interface.vm_nic : name => vm.private_ip_address
+  }
 }
 output "checker" {
-  value = azurerm_public_ip.vm_pip["checker"]._ip_address
+  value = azurerm_public_ip.vm_pip["checker"].ip_address
 }
 output "engine" {
-  value = azurerm_public_ip.vm_pip["engine"]._ip_address
+  value = azurerm_public_ip.vm_pip["engine"].ip_address
 }
 output "vulnbox1" {
-  value = azurerm_public_ip.vm_pip["vulnbox1"]._ip_address
+  value = azurerm_public_ip.vm_pip["vulnbox1"].ip_address
 }
