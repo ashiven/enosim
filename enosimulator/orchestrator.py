c4c3679a6c92438f1e579bf930f3e443387e16e9
ashiven <nevisha@pm.me>
2023-09-27 22:31:47 +0200

    Disable deployment and building infrastructure

--- enosimulator/orchestrator.py
+++ enosimulator/orchestrator.py
@@ -81,9 +81,9 @@ class Orchestrator:
         self.session.mount("http://", HTTPAdapter(max_retries=retry_strategy))
 
     def update_teams(self):
-        for service in self.setup.services:
+        for settings in self.setup.services.values():
             # Get service info from checker
-            checker_address = service.checkers[0]
+            checker_address = settings["checkers"][0]
             response = self.session.get(f"{checker_address}/service")
             if response.status_code != 200:
                 raise Exception("Failed to get service info from checker")
@@ -94,15 +94,15 @@ class Orchestrator:
             )
 
             # Update Exploiting / Patched categories for each team
-            for team in self.setup.teams:
-                team["exploiting"].update({info.service.name: {}})
-                team["patched"].update({info.service.name: {}})
+            for settings in self.setup.teams.values():
+                settings["exploiting"].update({info.service_name: {}})
+                settings["patched"].update({info.service_name: {}})
                 for flagstore_id in range(1, info.exploit_variants + 1):
-                    team["exploiting"][info.service_name].update(
-                        {f"{service.name}-Flag{flagstore_id}": False}
+                    settings["exploiting"][info.service_name].update(
+                        {f"Flag{flagstore_id}": False}
                     )
-                    team["patched"][info.service_name].update(
-                        {f"{service.name}-Flag{flagstore_id}": False}
+                    settings["patched"][info.service_name].update(
+                        {f"Flag{flagstore_id}": False}
                     )
 
     def send_exploit(source_team, target_team, service, flagstore):
