a34565def0dab43dac7cad15c3784742f5869d63
ashiven <nevisha@pm.me>
2023-09-29 20:05:51 +0200

    Refactor team adjustment in Simulation class

--- enosimulator/sim/sim.py
+++ enosimulator/sim/sim.py
@@ -25,6 +25,21 @@ def _exploit_or_patch(team):
     return random_variant, random_service, random_flagstore
 
 
+def _adjust_team(setup, team_name, variant, service, flagstore):
+    if (
+        variant == "exploiting"
+        and not setup.teams[team_name].exploiting[service][flagstore]
+    ):
+        setup.teams[team_name].exploiting[service][flagstore] = True
+        info_text = "started exploiting"
+    elif (
+        variant == "patched" and not setup.teams[team_name].patched[service][flagstore]
+    ):
+        setup.teams[team_name].patched[service][flagstore] = True
+        info_text = "patched"
+    print(Fore.GREEN + f"\n[+] Team {team_name} {info_text} {service}: {flagstore}\n")
+
+
 #### End Helpers ####
 
 
@@ -49,26 +64,7 @@ class Simulation:
             for team_name, team in self.setup.teams.items():
                 if _random_test(team):
                     variant, service, flagstore = _exploit_or_patch(team)
-                    if (
-                        variant == "exploiting"
-                        and not self.setup.teams[team_name].exploiting[service][
-                            flagstore
-                        ]
-                    ):
-                        self.setup.teams[team_name].exploiting[service][
-                            flagstore
-                        ] = True
-                        info_text = "started exploiting"
-                    elif (
-                        variant == "patched"
-                        and not self.setup.teams[team_name].patched[service][flagstore]
-                    ):
-                        self.setup.teams[team_name].patched[service][flagstore] = True
-                        info_text = "patched"
-                    print(
-                        Fore.GREEN
-                        + f"\n[+] Team {team_name} {info_text} {service}: {flagstore}\n"
-                    )
+                    _adjust_team(self.setup, team_name, variant, service, flagstore)
 
             # TODO:
             # - it may be a good idea to do this concurrently for each team
