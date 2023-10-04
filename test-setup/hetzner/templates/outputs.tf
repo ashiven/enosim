44951c7a2b545eed5dec7f829f8a34b350ccfc7a
ashiven <nevisha@pm.me>
2023-10-04 10:33:00 +0200

    Update outputs.tf to include vulnbox, checker, and engine public IP addresses

--- test-setup/hetzner/templates/outputs.tf
+++ test-setup/hetzner/templates/outputs.tf
@@ -1,6 +1,18 @@
-output "public_ip_addresses" {
+output "vulnbox_public_ip_addresses" {
   value = {
-    for vm in hcloud_server.vm :
-    vm.name => vm.ipv4_address
+    for server in hcloud_server.vulnbox_vm :
+    server.name => server.ipv4_address
+  }
+}
+output "checker_public_ip_addresses" {
+  value = {
+    for server in hcloud_server.checker_vm :
+    server.name => server.ipv4_address
+  }
+}
+output "engine_public_ip_address" {
+  value = {
+    for server in hcloud_server.engine_vm :
+    server.name => server.ipv4_address
   }
 }
