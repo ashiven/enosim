f7bb4854fc817d2b519692f24e6607df5269b7e0
ashiven <nevisha@pm.me>
2023-10-04 10:42:39 +0200

    Rename output variable "chccker_public_ips" to "checker_public_ips" in outputs.tf file

--- test-setup/hetzner/templates/outputs.tf
+++ test-setup/hetzner/templates/outputs.tf
@@ -1,7 +1,7 @@
 output "vulnbox_public_ips" {
   value = ["${hcloud_server.vulnbox_vm.*.ipv4_address}"]
 }
-output "chccker_public_ips" {
+output "checker_public_ips" {
   value = ["${hcloud_server.checker_vm.*.ipv4_address}"]
 }
 output "engine_public_ips" {
