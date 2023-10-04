9a8c7e6bd7fb9903ea76fc0c213e91b197107dc2
ashiven <nevisha@pm.me>
2023-10-04 10:40:55 +0200

    Refactor outputs.tf file for simplified IP address outputs

--- test-setup/hetzner/templates/outputs.tf
+++ test-setup/hetzner/templates/outputs.tf
@@ -1,18 +1,10 @@
-output "vulnbox_public_ip_addresses" {
-  value = {
-    for server in hcloud_server.vulnbox_vm :
-    server.name => server.ipv4_address
-  }
+output "vulnbox_public_ips" {
+  value = ["${hcloud_server.vulnbox_vm.*.ipv4_address}"]
 }
-output "checker_public_ip_addresses" {
-  value = {
-    for server in hcloud_server.checker_vm :
-    server.name => server.ipv4_address
-  }
+output "chccker_public_ips" {
+  value = ["${hcloud_server.checker_vm.*.ipv4_address}"]
 }
-output "engine_public_ip_address" {
-  value = {
-    for server in hcloud_server.engine_vm :
-    server.name => server.ipv4_address
-  }
+output "engine_public_ips" {
+  value = ["${hcloud_server.engine_vm.*.ipv4_address}"]
 }
+
