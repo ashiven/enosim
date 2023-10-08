07a86dc0c44e98536815da8471f422c2802a8769
ashiven <nevisha@pm.me>
2023-10-08 09:46:43 +0200

    add components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@components/ui/button"
+import { ModeToggle } from "@components/ui/toggle"
+
 export default function Home() {
-   return <main className=""></main>
+   return (
+      <main className="">
+         <Button>Hello World</Button>
+         <Button>Bye World</Button>
+         <ModeToggle />
+      </main>
+   )
 }
