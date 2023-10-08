03052c93b27d71f28b1df5651a80615ccef37846
ashiven <nevisha@pm.me>
2023-10-08 13:47:43 +0200

    Refactor VMCharts component to use named export instead of default export

--- frontend/components/vmcharts.tsx
+++ frontend/components/vmcharts.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/menubar"
 import { MyAreaGraph } from "@components/charts/areagraph"
 import { MyBarChart } from "@components/charts/barchart"
+import React from "react"
 
 /* Start sample data for bar charts and area charts */
 import { useApiUrl, useCustom } from "@refinedev/core"
@@ -27,7 +28,7 @@ const query = {
 }
 /* End sample data for bar charts and area charts */
 
-export default function VMCharts() {
+export const VMCharts: React.FC = () => {
    /* Start sample data for bar charts and area charts */
    const API_URL = useApiUrl()
 
