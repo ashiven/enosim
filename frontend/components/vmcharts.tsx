5020c9dfd7a347748a2cc9e1fcca2e5b3e9604ba
ashiven <nevisha@pm.me>
2023-10-08 14:30:20 +0200

    Fill VMCharts data fields with actual data

--- frontend/components/vmcharts.tsx
+++ frontend/components/vmcharts.tsx
@@ -4,53 +4,81 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { MyAreaGraph } from "@components/charts/areagraph"
 import { MyBarChart } from "@components/charts/barchart"
 
+{
+   /* TODO: - the data fields need to be filled with actual data */
+}
 export default function VMCharts() {
    return (
-      <Card className="p-5">
-         {/* Some Charts with performance info */}
-         <Tabs defaultValue="revenue">
-            <TabsList>
-               <TabsTrigger value="revenue">Revenue</TabsTrigger>
-               <TabsTrigger value="orders">Orders</TabsTrigger>
-               <TabsTrigger value="customers">Customers</TabsTrigger>
-            </TabsList>
-            {/* Revenue chart */}
-            <TabsContent value="revenue">
-               Revenue
-               <Card>
-                  <CardContent>
-                     {/* TODO: - the data fields need to be filled with actual data */}
-                     <MyAreaGraph
-                        data={[
-                           { date: "2021-12-12", value: "20" },
-                           { date: "2021-12-13", value: "30" },
-                           { date: "2021-12-14", value: "40" },
-                        ]}
-                        stroke="#8884d8"
-                        fill="#cfeafc"
-                     />
-                  </CardContent>
-               </Card>
-            </TabsContent>
-            {/* Orders chart */}
-            <TabsContent value="orders">
-               Orders
-               <Card>
-                  <CardContent>
-                     <MyBarChart data={[]} fill="#ffce90" />
-                  </CardContent>
-               </Card>
-            </TabsContent>
-            {/* Customers chart */}
-            <TabsContent value="customers">
-               Customers
-               <Card>
-                  <CardContent>
-                     <MyAreaGraph data={[]} stroke="#00bd56" fill="#ccf3f3" />
-                  </CardContent>
-               </Card>
-            </TabsContent>
-         </Tabs>
-      </Card>
+      <div className="p-5">
+         <Card className="p-5">
+            {/* Some Charts with performance info */}
+            <Tabs defaultValue="revenue">
+               <TabsList>
+                  <TabsTrigger value="revenue">Revenue</TabsTrigger>
+                  <TabsTrigger value="orders">Orders</TabsTrigger>
+                  <TabsTrigger value="customers">Customers</TabsTrigger>
+               </TabsList>
+               {/* Revenue chart */}
+               <TabsContent value="revenue">
+                  Revenue
+                  <Card>
+                     <CardContent>
+                        <MyAreaGraph
+                           data={[
+                              { date: "2021-12-12", value: "20" },
+                              { date: "2021-12-13", value: "30" },
+                              { date: "2021-12-14", value: "40" },
+                              { date: "2021-12-15", value: "30" },
+                              { date: "2021-12-16", value: "35" },
+                              { date: "2021-12-17", value: "50" },
+                           ]}
+                           stroke="#8884d8"
+                           fill="#cfeafc"
+                        />
+                     </CardContent>
+                  </Card>
+               </TabsContent>
+               {/* Orders chart */}
+               <TabsContent value="orders">
+                  Orders
+                  <Card>
+                     <CardContent>
+                        <MyBarChart
+                           data={[
+                              { date: "2021-12-12", value: "20" },
+                              { date: "2021-12-13", value: "30" },
+                              { date: "2021-12-14", value: "40" },
+                              { date: "2021-12-15", value: "30" },
+                              { date: "2021-12-16", value: "35" },
+                              { date: "2021-12-17", value: "50" },
+                           ]}
+                           fill="#ffce90"
+                        />
+                     </CardContent>
+                  </Card>
+               </TabsContent>
+               {/* Customers chart */}
+               <TabsContent value="customers">
+                  Customers
+                  <Card>
+                     <CardContent>
+                        <MyAreaGraph
+                           data={[
+                              { date: "2021-12-12", value: "20" },
+                              { date: "2021-12-13", value: "30" },
+                              { date: "2021-12-14", value: "40" },
+                              { date: "2021-12-15", value: "30" },
+                              { date: "2021-12-16", value: "35" },
+                              { date: "2021-12-17", value: "50" },
+                           ]}
+                           stroke="#00bd56"
+                           fill="#ccf3f3"
+                        />
+                     </CardContent>
+                  </Card>
+               </TabsContent>
+            </Tabs>
+         </Card>
+      </div>
    )
 }
