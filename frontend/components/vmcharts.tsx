b81ddac0bacf20e77b26da3729686110c4f76802
ashiven <nevisha@pm.me>
2023-10-08 14:27:05 +0200

    Add tabs component for charts in VMCharts

--- frontend/components/vmcharts.tsx
+++ frontend/components/vmcharts.tsx
@@ -1,12 +1,6 @@
 "use client"
 import { Card, CardContent } from "@/components/ui/card"
-import {
-   Menubar,
-   MenubarContent,
-   MenubarItem,
-   MenubarMenu,
-   MenubarTrigger,
-} from "@/components/ui/menubar"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { MyAreaGraph } from "@components/charts/areagraph"
 import { MyBarChart } from "@components/charts/barchart"
 
@@ -14,54 +8,49 @@ export default function VMCharts() {
    return (
       <Card className="p-5">
          {/* Some Charts with performance info */}
-         <Menubar>
-            <MenubarMenu>
-               <MenubarTrigger>File</MenubarTrigger>
-               <MenubarContent aria-label="Options" className="gap-0">
-                  {/* Revenue chart */}
-                  <MenubarItem>
-                     Hello
-                     <Card>
-                        <CardContent>
-                           {/* TODO: - the data fields need to be filled with actual data */}
-                           <MyAreaGraph
-                              data={[
-                                 { date: "2021-12-12", value: "20" },
-                                 { date: "2021-12-13", value: "30" },
-                                 { date: "2021-12-14", value: "40" },
-                              ]}
-                              stroke="#8884d8"
-                              fill="#cfeafc"
-                           />
-                        </CardContent>
-                     </Card>
-                  </MenubarItem>
-                  {/* Orders chart */}
-                  <MenubarItem>
-                     {" "}
-                     Why are you gay?
-                     <Card>
-                        <CardContent>
-                           <MyBarChart data={[]} fill="#ffce90" />
-                        </CardContent>
-                     </Card>
-                  </MenubarItem>
-                  {/* Customers chart */}
-                  <MenubarItem>
-                     Dummy
-                     <Card>
-                        <CardContent>
-                           <MyAreaGraph
-                              data={[]}
-                              stroke="#00bd56"
-                              fill="#ccf3f3"
-                           />
-                        </CardContent>
-                     </Card>
-                  </MenubarItem>
-               </MenubarContent>
-            </MenubarMenu>
-         </Menubar>
+         <Tabs defaultValue="revenue">
+            <TabsList>
+               <TabsTrigger value="revenue">Revenue</TabsTrigger>
+               <TabsTrigger value="orders">Orders</TabsTrigger>
+               <TabsTrigger value="customers">Customers</TabsTrigger>
+            </TabsList>
+            {/* Revenue chart */}
+            <TabsContent value="revenue">
+               Revenue
+               <Card>
+                  <CardContent>
+                     {/* TODO: - the data fields need to be filled with actual data */}
+                     <MyAreaGraph
+                        data={[
+                           { date: "2021-12-12", value: "20" },
+                           { date: "2021-12-13", value: "30" },
+                           { date: "2021-12-14", value: "40" },
+                        ]}
+                        stroke="#8884d8"
+                        fill="#cfeafc"
+                     />
+                  </CardContent>
+               </Card>
+            </TabsContent>
+            {/* Orders chart */}
+            <TabsContent value="orders">
+               Orders
+               <Card>
+                  <CardContent>
+                     <MyBarChart data={[]} fill="#ffce90" />
+                  </CardContent>
+               </Card>
+            </TabsContent>
+            {/* Customers chart */}
+            <TabsContent value="customers">
+               Customers
+               <Card>
+                  <CardContent>
+                     <MyAreaGraph data={[]} stroke="#00bd56" fill="#ccf3f3" />
+                  </CardContent>
+               </Card>
+            </TabsContent>
+         </Tabs>
       </Card>
    )
 }
