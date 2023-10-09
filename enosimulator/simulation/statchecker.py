dc844c153b3f5455fe893ee985094468b180bb0c
ashiven <nevisha@pm.me>
2023-10-09 12:44:36 +0200

    Update SSH_PORT to 22 in check_containers method

--- enosimulator/simulation/statchecker.py
+++ enosimulator/simulation/statchecker.py
@@ -20,7 +20,7 @@ class StatChecker:
         self.console = Console()
 
     def check_containers(self, ip_address):
-        SSH_PORT = 2375
+        SSH_PORT = 22
         SSH_USER = self.usernames[SetupVariant.from_str(self.config.setup.location)]
         client = docker.DockerClient(
             base_url=f"ssh://{SSH_USER}@{ip_address}:{SSH_PORT}"
