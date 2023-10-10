6e9d9cfc41e99c9f2f7120fc4f669e6659da88e7
ashiven <nevisha@pm.me>
2023-10-10 15:37:00 +0200

    Add VMCard component and update ServiceCard component

--- frontend/components/services/servicecard.tsx
+++ frontend/components/services/servicecard.tsx
@@ -17,30 +17,28 @@ interface ServiceCardProps {
 export default function ServiceCard(props: ServiceCardProps) {
    return (
       <HoverCard>
-         <HoverCardTrigger>
-            <Card>
-               <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                     {props.name}
-                  </CardTitle>
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 24 24"
-                     fill="none"
-                     stroke="currentColor"
-                     strokeLinecap="round"
-                     strokeLinejoin="round"
-                     strokeWidth="2"
-                     className="h-4 w-4 text-muted-foreground"
-                  >
-                     <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                  </svg>
-               </CardHeader>
-               <CardContent>
-                  <div className="text-2xl font-bold">+573</div>
-               </CardContent>
-            </Card>
-         </HoverCardTrigger>
+         <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+               <CardTitle className="text-sm font-medium">
+                  <HoverCardTrigger>{props.name}</HoverCardTrigger>
+               </CardTitle>
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="h-4 w-4 text-muted-foreground"
+               >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+               </svg>
+            </CardHeader>
+            <CardContent>
+               <div className="text-2xl font-bold">+573</div>
+            </CardContent>
+         </Card>
          <HoverCardContent>
             <span className="font-bold">Id: </span>
             <span>{props.id}</span>
