85609b83f86b9c4777e02a888f154e8736167696
ashiven <nevisha@pm.me>
2023-10-13 19:50:19 +0200

    Add selenium web scraping to test-parse-scoreboard.py

--- test-parse-scoreboard.py
+++ test-parse-scoreboard.py
@@ -1,15 +1,26 @@
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 url = "http://5.75.163.243:5001/scoreboard"
-r = requests.get(url)
-print(r.text)
-soup = BeautifulSoup(r.text, "html.parser")
-print(soup)
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(url)
 
+
+wait = WebDriverWait(driver, 10)
+wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "otherrow")))
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
 rows = soup.find_all("td", class_="otherrow")
+
+print(rows)
 
 for row in rows:
     score = row.find("td", class_="team-score").text.strip()
     team_name = row.find("div", class_="team-name").find("a").text.strip()
     print(score, team_name)
+
+driver.quit()
