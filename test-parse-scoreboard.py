148993ca79169a5ee2da00634a34dea6915d69d1
ashiven <nevisha@pm.me>
2023-10-13 19:59:33 +0200

    Update test-parse-scoreboard.py to print team name, score, and gain for each row in the scoreboard

--- test-parse-scoreboard.py
+++ test-parse-scoreboard.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 url = "http://5.75.163.243:5001/scoreboard"
-driver = webdriver.Chrome(ChromeDriverManager().install())
+
+options = Options()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.get(url)
 
 
@@ -14,13 +18,11 @@ wait = WebDriverWait(driver, 10)
 wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "otherrow")))
 
 soup = BeautifulSoup(driver.page_source, "html.parser")
-rows = soup.find_all("td", class_="otherrow")
-
-print(rows)
+rows = soup.find_all("tr", class_="otherrow")
 
 for row in rows:
-    score = row.find("td", class_="team-score").text.strip()
+    [score, gain] = row.find("td", class_="team-score").text.strip().split(" ")
     team_name = row.find("div", class_="team-name").find("a").text.strip()
-    print(score, team_name)
+    print(team_name, score, gain[2:-1])
 
 driver.quit()
