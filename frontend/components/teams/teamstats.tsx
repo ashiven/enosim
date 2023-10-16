1c287c74ef96acb363cf8f8efee4a03793ea5a10
ashiven <nevisha@pm.me>
2023-10-16 14:03:51 +0200

    Update hetzner.config.json duration-in-minutes to 300 and refactor teamstats.tsx to use getServerSideProps

--- frontend/components/teams/teamstats.tsx
+++ frontend/components/teams/teamstats.tsx
@@ -1,21 +1,18 @@
 import TeamCard from "@/components/teams/teamcard"
 
-async function getData() {
+export async function getServerSideProps() {
    const res = await fetch(`http://127.0.0.1:5000/teams`)
-   if (!res.ok) {
-      throw new Error("Failed to fetch data")
-   }
-   return res.json()
+   const data = await res.json()
+
+   return { props: { data } }
 }
 
-export default async function TeamStats() {
-   const teamJson = await getData()
-
+export default function TeamStats({ data }: any) {
    return (
       <div className="container mx-auto mt-12 mb-8">
          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            {Object.keys(teamJson).map((team: string) => (
-               <TeamCard data={teamJson[team]} />
+            {Object.keys(data).map((team: string) => (
+               <TeamCard data={data[team]} />
             ))}
          </div>
       </div>
