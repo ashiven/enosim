13fd039750228d12bd4bfb508398a18145997b16
ashiven <nevisha@pm.me>
2023-10-16 14:45:27 +0200

    Fix stroke-width attribute and rename stroke-linecap and stroke-linejoin attributes in VMCard component

--- frontend/components/overview/vmcard.tsx
+++ frontend/components/overview/vmcard.tsx
@@ -33,11 +33,11 @@ export default function VMCard(props: VMCardProps) {
                      xmlns="http://www.w3.org/2000/svg"
                      className="h-4 w-4 text-muted-foreground"
                      viewBox="0 0 24 24"
-                     stroke-width="1.5"
+                     strokeWidth="1.5"
                      stroke="#000000"
                      fill="none"
-                     stroke-linecap="round"
-                     stroke-linejoin="round"
+                     strokeLinecap="round"
+                     strokeLinejoin="round"
                   >
                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                      <path d="M5 5m0 1a1 1 0 0 1 1 -1h12a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1h-12a1 1 0 0 1 -1 -1z" />
