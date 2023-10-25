78b261ebe6755144b47598ebf50edc6c646c3bc0
ashiven <nevisha@pm.me>
2023-10-25 16:01:57 +0200

    fix chromedriver options

--- enosimulator/simulation/orchestrator.py
+++ enosimulator/simulation/orchestrator.py
@@ -158,7 +158,9 @@ class Orchestrator:
         )
 
         options = Options()
-        options.headless = True
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()), options=options
