73aa3a92de670556a9a57a3dc83e87598ba29a55
ashiven <nevisha@pm.me>
2023-10-30 15:39:30 +0100

    Add container_single.py, main.py, and scoreboard_analysis.py

---
+++ enosimulator/container_single.py
@@ -0,0 +1,94 @@
+from threading import Lock
+
+from backend.app import FlaskApp
+from dependency_injector import containers, providers
+from httpx import AsyncClient
+from rich.console import Console
+from setup.setup import Setup
+from setup.setup_helper.setup_helper import SetupHelper, TeamGenerator
+from simulation.flagsubmitter import FlagSubmitter
+from simulation.orchestrator import Orchestrator
+from simulation.simulation import Simulation
+from simulation.statchecker import StatChecker
+
+
+class Container(containers.DeclarativeContainer):
+    config = providers.Configuration()
+
+    # Util classes (console, client, locks, etc.)
+
+    console = providers.Factory(Console)
+    client = providers.Factory(AsyncClient)
+    thread_lock = providers.Factory(Lock)
+    locks = providers.Singleton(
+        dict,
+        service_lock=thread_lock,
+        team_lock=thread_lock,
+        round_info_lock=thread_lock,
+    )
+
+    # Setup
+
+    team_gen = providers.Factory(TeamGenerator, config=config.config)
+
+    setup_helper = providers.Factory(
+        SetupHelper,
+        config=config.config,
+        secrets=config.secrets,
+        team_gen=team_gen,
+    )
+
+    setup = providers.Singleton(
+        Setup,
+        config=config.config,
+        secrets=config.secrets,
+        setup_helper=setup_helper,
+        console=console,
+    )
+
+    # Simulation
+
+    flag_submitter = providers.Factory(
+        FlagSubmitter,
+        setup=setup,
+        console=console,
+        verbose=config.verbose,
+        debug=config.debug,
+    )
+
+    stat_checker = providers.Factory(
+        StatChecker,
+        config=config.config,
+        secrets=config.secrets,
+        client=client,
+        console=console,
+        verbose=config.verbose,
+    )
+
+    orchestrator = providers.Factory(
+        Orchestrator,
+        setup=setup,
+        locks=locks,
+        client=client,
+        flag_submitter=flag_submitter,
+        stat_checker=stat_checker,
+        console=console,
+        verbose=config.verbose,
+        debug=config.debug,
+    )
+
+    simulation = providers.Singleton(
+        Simulation,
+        setup=setup,
+        orchestrator=orchestrator,
+        locks=locks,
+        console=console,
+        verbose=config.verbose,
+        debug=config.debug,
+    )
+
+    # Flask
+
+    flask_app = providers.Factory(
+        FlaskApp, setup=setup, simulation=simulation, locks=locks
+    )
--- enosimulator/main.py
+++ enosimulator/main.py
@@ -5,7 +5,7 @@ import sys
 from threading import Thread
 
 from backend.app import FlaskApp
-from containers import Application
+from container_single import Container
 from dependency_injector.wiring import Provide, inject
 from dotenv import load_dotenv
 from setup.setup import Setup
@@ -72,10 +72,10 @@ def get_args() -> argparse.Namespace:
 
 @inject
 async def main(
-    destroy: bool = Provide[Application.config.destroy],
-    setup: Setup = Provide[Application.setup_container.setup],
-    simulation: Simulation = Provide[Application.simulation_container.simulation],
-    app: FlaskApp = Provide[Application.backend_container.flask_app],
+    destroy: bool = Provide[Container.config.destroy],
+    setup: Setup = Provide[Container.setup],
+    simulation: Simulation = Provide[Container.simulation],
+    app: FlaskApp = Provide[Container.flask_app],
 ) -> None:
     if destroy:
         setup.destroy()
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     sys.path.append("../..")
     args = get_args()
 
-    container = Application()
+    container = Container()
     container.config.config.from_json(args.config)
     container.config.secrets.from_json(args.secrets)
     container.config.verbose = args.verbose
