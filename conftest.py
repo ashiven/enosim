2ecd69a5f2d2df52bf25791170f6063bdb7757f6
ashiven <nevisha@pm.me>
2023-11-01 16:30:58 +0100

    Refactor .coveragerc and conftest.py files

--- conftest.py
+++ conftest.py
@@ -2,6 +2,8 @@ from pytest import fixture
 
 from enosimulator.containers import SetupContainer
 
+collect_ignore = ["setup.py"]
+
 config = {
     "setup": {
         "ssh-config-path": "C:/Users/janni/.ssh/simconfig",
